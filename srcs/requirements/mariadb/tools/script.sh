6ed45a0f2d0eaf947b66f018daca349411a2af1f
Miguel Silva <miguel198282@gmail.com>
2024-03-25 15:12:08 +0000

    FINISHED AT LEAST

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
-if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
-
 service mariadb start;
-
 sleep 3;
 
-
 echo "Creating admin user"
-mysql -u root -e "CREATE DATABASE $DB_NAME;"
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 mysql -u root -e "CREATE USER '$DB_ADMIN'@'%' IDENTIFIED BY '$DB_PASS';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_ADMIN'@'%';"
 mysql -u root -e "FLUSH PRIVILEGES;"
 
 echo "Setting password for root user"
 mysqladmin -u root password "$DB_PASS"
-
 mysqladmin -u root -p"$DB_PASS" shutdown
-fi
 
 exec "$@"
